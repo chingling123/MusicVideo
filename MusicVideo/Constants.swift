1e2f71dd5428457d001fcf9791e440483352b8af
Erik Nascimento <erik@desconcerto.com.br>
2016-02-14 18:03:24 -0200

    Part 4 Completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Erik Nascimento on 2/14/16.
+//  Copyright © 2016 2EG. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
